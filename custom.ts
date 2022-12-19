69db18bc6748d78739ea5bcb7bbd386fdf37b4e7
Ralf Krause <ralf.krause@gmail.com>
2022-12-19 13:33:07 +0100

    clean the code

--- custom.ts
+++ custom.ts
@@ -1,38 +1,22 @@
-
 /**
 * Nutze diese Datei für benutzerdefinierte Funktionen und Blöcke.
 * Weitere Informationen unter https://makecode.calliope.cc/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
-    One,
-    //% block="two"
-    Two
-}
-
 /**
- * Custom blocks
+ * Animation blocks
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace blinker {
+namespace Animations {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Blink an led at a particular interval
+     * @param x horizontal led coordinate 0..4
+     * @param y vertical led coordinate 0..4
+     * @param interval time in milliseconds between blinks
      */
-    //% block ="blink led x $x y $y with interval $interval"
+    //% block ="blink led x $x y $y every $interval ms"
     export function blink(x: number, y: number, interval: number): void {
         // Add code here
     }
-
-    /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
-     */
-    //% block
-    export function fib(value: number): number {
-        return value <= 1 ? value : fib(value -1) + fib(value - 2);
-    }
+    
 }
