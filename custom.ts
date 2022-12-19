a42273b31377dea34c00ae16fd34d15b0bdf29bb
Ralf Krause <ralf.krause@gmail.com>
2022-12-19 12:53:37 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ custom.ts
@@ -0,0 +1,38 @@
+
+/**
+* Nutze diese Datei für benutzerdefinierte Funktionen und Blöcke.
+* Weitere Informationen unter https://makecode.calliope.cc/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace blinker {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block ="blink led x $x y $y with interval $interval"
+    export function blink(x: number, y: number, interval: number): void {
+        // Add code here
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function fib(value: number): number {
+        return value <= 1 ? value : fib(value -1) + fib(value - 2);
+    }
+}
