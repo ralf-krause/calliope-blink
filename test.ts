0970a569fa8cd1d45d0a51575bcc51c34797d019
Ralf Krause <ralf.krause@gmail.com>
2022-12-19 15:54:28 +0100

    changing text in text.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
 // Hier kann man Tests durchführen
-// Diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+// Diese Datei wird nicht kompiliert, wenn das Paket als Erweiterung verwendet wird.
 for (let i=0; i < 5; ++i)
     animations.blink(i, i, 100 + i * 100)
